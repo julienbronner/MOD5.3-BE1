74f70dd1b1a5843b3abca9e5293f1f12fdad2f55
denis <lks.denis@gmail.com>
2020-10-21 10:26:43 +0200

    Update BE1-segmentation_de_video.py
    Construction du tableau de données visuelles

--- BE1-segmentation_de_video.py
+++ BE1-segmentation_de_video.py
@@ -6,22 +6,33 @@ Created on Wed Oct 21 08:24:53 2020
 """
 
 import cv2
+import numpy as np
+
+# Construct a VideoReader object associated with the sample file.
 
 vidObj = cv2.VideoCapture('Pub_C+_176_144.mp4')
-success,image = vidObj.read()
-count = 0
-print(success)
-while success:
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidObj.read()
-  #print('Read a new frame: ', success)
-  count += 1
-  
 
-# pour les dimensions
+# Determine the height and width of the frames.
+if (vidObj.isOpened()== False): 
+	  print("Error opening video stream or file")
+
 if vidObj.isOpened(): 
 
-    width  = vidObj.get(3) # float
-    height = vidObj.get(4) # float
+    nb_frames = int(vidObj.get(7))
+    vidWidth  = int(vidObj.get(3))
+    vidHeight = int(vidObj.get(4))
 
-    print('width, height:', width, height)
+    print('Largeur : ', vidWidth, ' ; Hauteur : ', vidHeight, " ; Nb d'images : ", nb_frames)
+
+# Create a Python movie structure array, Mat.
+Mat = np.zeros((nb_frames, vidHeight, vidWidth, 3))
+
+success,image = vidObj.read()
+count = 0
+success = True
+while success:
+  success,image = vidObj.read()
+  Mat[count] = image         # save each frame into Mat
+  count += 1
+  if count >= 10:
+      break
