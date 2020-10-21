1303ad8f79c0ed4058fa99e0e7997f84b884284f
denis <lks.denis@gmail.com>
2020-10-21 12:15:29 +0200

    Construction histogramme

--- BE1-segmentation_de_video.py
+++ BE1-segmentation_de_video.py
@@ -6,6 +6,7 @@ Created on Wed Oct 21 08:24:53 2020
 """
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Construct a VideoReader object associated with the sample file.
 
@@ -33,5 +34,26 @@ else:
       success,image = vidObj.read()
       Mat[count] = image         # save each frame into Mat
       count += 1
-      if count >= 10:
+      if count >= 50:
           break
+
+Red = []
+Green = []
+Blue = []
+
+for i in range(50):
+    r, g, b = 0, 0, 0
+    for j in range(vidHeight):
+        for k in range(vidWidth):
+            r += Mat[i][j][k][0]
+            g += Mat[i][j][k][1]
+            b += Mat[i][j][k][2]
+    Red.append(r)
+    Green.append(g)
+    Blue.append(b)
+    
+plt.hist([Red, Green, Blue], bins = [x for x in range(0, 50)], color = ['red', 'green', 'blue'],
+         label = ['Red', 'Green', 'Blue']) # bar est le defaut
+plt.ylabel('Amount')
+plt.xlabel('Frame')
+plt.legend()
