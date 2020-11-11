9f70e261e693adcdcb4b972a4d5cf40e7456efa5
denis <lks.denis@gmail.com>
2020-11-11 23:30:38 +0100

    Update BE1-segmentation_de_video_split.py

--- BE1-segmentation_de_video_split.py
+++ BE1-segmentation_de_video_split.py
@@ -38,7 +38,6 @@ def Get_Image_colour(vidObj, limit, vidWidth, vidHeight, split = 1):
                                        )/(vidWidth*vidHeight/split**2))
                 else :
                     break
-        #print(Red)
         if len(Red) == split**2:
             if count == 0 :
                 Red_split[0] = Red
@@ -61,26 +60,32 @@ def Get_Image_greyscale(vidObj, limit, vidWidth, vidHeight):
     
     while count < limit :
         success,image = vidObj.read()
+        Grey = []
         for i in range(0, split):
             for j in range(0, split):
                 if success:
                     Grey.append(np.sum(image[int(i*vidWidth/split):
                                              int((i+1)*vidWidth/split),
                                              int(j*vidHeight/split):
-                                             int((j+1)*vidHeight/split),0]/3 +
+                                             int((j+1)*vidHeight/split),0]*0.11 +
                                        image[int(i*vidWidth/split):
                                              int((i+1)*vidWidth/split),
                                              int(j*vidHeight/split):
-                                             int((j+1)*vidHeight/split),1]/3 +
-                                           image[int(i*vidWidth/split):
+                                             int((j+1)*vidHeight/split),1]*0.59 +
+                                       image[int(i*vidWidth/split):
                                              int((i+1)*vidWidth/split),
                                              int(j*vidHeight/split):
-                                             int((j+1)*vidHeight/split),2]/3)
-                                /(vidWidth*vidHeight/split**2))     
-        else :
-            break
+                                             int((j+1)*vidHeight/split),2]*0.3)
+                                /(vidWidth*vidHeight/split**2))   
+                else :
+                    break
+        if len(Grey) == split**2:
+            if count == 0 :
+                Grey_split[0] = Grey
+            else :
+                Grey_split = np.append(Grey_split, [Grey], axis = 0)
         count += 1
-    return Grey
+    return Grey_split
 
 def Analyse_colour(Nb_f_pos, Nb_f_neg, Nb_erreur, seuil_cut, seuil_noir, cut):
     Noir = []
@@ -211,19 +216,22 @@ else:
     Cut_verif = {52, 142, 163, 187, 200, 221, 248, 256, 268, 307, 485, 526, 561, 582, 595, 615, 635, 664, 690, 705, 720, 746, 821, 853, 903, 956, 975, 998, 1027, 1062, 1099, 1120, 1144, 1177, 1220, 1255, 1293, 1335, 1367, 1444, 1582, 1655, 1735, 1812, 1871, 1895, 1909, 1960, 2016, 2106, 2147, 2184, 2243, 2487, 2526, 2617, 2688, 2775, 2808, 2829, 2858, 2881, 2917, 2934, 2962, 2978, 3011, 3086, 3179}
     Noir_verif = {42, 552, 812, 1573, 2007, 2766, 3277}
     
-    GreyScale = False
+    GreyScale = True
     displayEvo = True
     split = 2
 
     if GreyScale :
-        Grey = Get_Image_greyscale(vidObj, limit, vidWidth, vidHeight)
+        Grey_split = Get_Image_greyscale(vidObj, limit, vidWidth, vidHeight)
         if displayEvo :
+            fig, axs = plt.subplots(split, split)
             Xrange = [x for x in range(0, min(limit, nb_frames-3))]
-            plt.plot(Xrange, Grey, label = "Niveaux de gris", color = 'grey')
+            for i in range(0, split):
+                for j in range(0, split):
+                    axs[i, j].plot(Xrange, Grey_split[:,i+j*2], label = "Niveaux de gris", color = 'grey')
             # plt.plot(Xrange, np.ones(len(Grey))*7.8, '-.', label  = 'Seuil de détection des noirs', color = 'black')
-            plt.xlabel("Images")
-            plt.ylabel("Quantification de l'intensité des gris")
-            plt.legend()
+            # plt.xlabel("Images")
+            # plt.ylabel("Quantification de l'intensité des gris")
+            # plt.legend()
             # plt.savefig('Evolution des niveaux de gris.png', dpi=300)
     else :
         Red_split, Green_split, Blue_split = Get_Image_colour(vidObj, limit, vidWidth, vidHeight, split)
