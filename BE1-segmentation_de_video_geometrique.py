8376e9caf416e4c743571fc7ae02aeea761efdaa
Julien Bronner <julbronner@free.fr>
2020-11-13 16:39:11 +0100

    ajout

--- BE1-segmentation_de_video_geometrique.py
+++ BE1-segmentation_de_video_geometrique.py
@@ -59,26 +59,33 @@ def image_to_greyscale(image):
     
 def filtrage_geo_image(image_greyscale, masque_filtrage):
     Height, Width = np.shape(image_greyscale)[:2]
-    image_filtree = np.zeros((Height-2, Width-2))
-    for i in range(1,Height-1):
-        for j in range(1,Width-1):
-            sous_matrice = image_greyscale[i-1:i+2, j-1:j+2]
-            image_filtree[i-1,j-1] = sum(sum(sous_matrice* masque_filtrage))
+    pad = (np.shape(masque_filtrage)[0] - 1) // 2
+    print(pad)
+    image_filtree = np.zeros((Height, Width))
+    image_greyscale = cv2.copyMakeBorder(image_greyscale, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
+    for i in range(pad, Height+pad):
+        for j in range(pad, Width+pad):
+            sous_matrice = image_greyscale[i-pad:i+pad+1, j-pad:j+pad+1]
+            image_filtree[i-pad,j-pad] = sum(sum(sous_matrice* masque_filtrage))
     image_filtree = rescale_intensity(image_filtree, in_range=(0, 255))
     image_filtree = (image_filtree * 255).astype("uint8")
     return image_filtree
 
-def elargissement_bord(image_filtree,seuil):
+def elargissement_bord(image_filtree,seuil): # fait un bord plus large et seuil pour etre soit blanc soit noir
     Height, Width = np.shape(image_filtree)
-    matrice_unite = np.ones((3,3))
+    matrice_unite = np.ones((5,5))
     image_bord = np.zeros((Height, Width))
-    pad = 1 #(kW - 1) // 2
-    image_bord = cv2.copyMakeBorder(image_bord, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
-    for i in range(1,Height):
-        for j in range(1,Width):
+    pad = (np.shape(matrice_unite)[0] - 1) // 2
+    image_filtree = cv2.copyMakeBorder(image_filtree, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
+    for i in range(pad, Height+pad):
+        for j in range(pad, Width+pad):
             if image_filtree[i,j] > seuil :
-                image_bord[i-1:i+2, j-1:j+2] = image_filtree[i,j]*matrice_unite
+                image_bord[i-pad:i+pad+1, j-pad:j+pad+1] = 255*matrice_unite #image_filtree[i,j]*matrice_unite
+            else : 
+                image_bord[i, j] = 0
     return image_bord
+
+
 
 #%% Main
 vidObj = cv2.VideoCapture('D:/julbr/Documents/ecole/ECL/3A/MOD 5.3 Analyse de données/BE1_Traitement_et_analyse_des_données_visuelles_et_sonores 2020-21/MOD5.3-BE1/Pub_C+_176_144.mp4')
@@ -102,5 +109,5 @@ image_path = "D:/julbr/Documents/ecole/ECL/3A/MOD 3.2 Deep Learning & IA/TD2/TD2
 gray = image_to_greyscale(image_path)
 filtre = filtrage_geo_image(gray, masque_base)
 plt.imshow(filtre, cmap = plt.get_cmap('gray'))
-bord = elargissement_bord(filtre, 150)
-plt.imshow(bord, cmap = plt.get_cmap('gray'))
+#bord = elargissement_bord(filtre, 150)
+#plt.imshow(bord, cmap = plt.get_cmap('gray'))
