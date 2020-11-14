3243e436fa1bd5ffdc64982348f1a9fca1efc02f
denis <lks.denis@gmail.com>
2020-11-14 18:42:55 +0100

    Update BE1-segmentation_de_video_split.py

--- BE1-segmentation_de_video_split.py
+++ BE1-segmentation_de_video_split.py
@@ -83,71 +83,61 @@ def Get_Image_greyscale(vidObj, limit, vidWidth, vidHeight):
         count += 1
     return Grey_split
 #%%
-def Analyse_colour(Nb_f_pos, Nb_f_neg, Nb_erreur, seuil_cut, seuil_noir, cut):
-    Noir = []
-    # On répèrtorie les cut détectés par des grandes variations de chaque couleurs
-    RedCut_split = []
-    GreenCut_split = []
-    BlueCut_split = []
+def Analyse_colour(Nb_f_pos_split, Nb_f_neg_split, Nb_erreur_split, seuil_cut, seuil_noir, cut):
     
-    RedCut = []
-    GreenCut = []
-    BlueCut = []
+    nb_f_pos =  [0 for i in range(split**2)]
+    nb_f_neg =  [0 for i in range(split**2)]
+    nb_erreur = [0 for i in range(split**2)]
     
-    for i in range(2,len(Red)):
-        if np.abs(Red[i]-Red[i-1]) > seuil_cut:
-            RedCut.append(i)
-            # print("Coupure des rouges à la frame ", i+1)
-        if np.abs(Green[i]-Green[i-1]) > seuil_cut:
-            GreenCut.append(i)
-        if np.abs(Blue[i]-Blue[i-1]) > seuil_cut:
-            BlueCut.append(i)
-        """
-        if Red[i] < seuil_noir and Green[i] < seuil_noir and Blue[i] < seuil_noir :
-            if len(Noir) == 0 or i > Noir[-1]+fps: # On considère qu'un plan noir dure moins de 0.5s
-                Noir.append(i+1)
-            # print("Plan noir à la frame ", i+1)
-            # print("Red : ",Red[i], "; Green : ", Green[i], "; Blue : ", Blue[i])
-        """
-    # Puis on considère que c'est un cut si les variations sont communes à chaque couleur
-    Cut = []
-    for i in RedCut:
-        # if i in GreenCut and i in BlueCut :
-        if Red[i] < seuil_noir and Green[i] < seuil_noir and Blue[i] < seuil_noir :
-            Noir.append(i+1)
-        else :
-            Cut.append(i+1)
-            
-            # print("Coupure à la frame ", i+1)
-            # print("Red : ",Red[i], "; Green : ", Green[i], "; Blue : ", Blue[i])
-                    
-    # On compare à présent les résultats à la vérité sur terrain
-    Noir = set(Noir)
-    Cut = set(Cut)
-    """
-    if len(Cut) == 0 :
-        print("Erreur : seuil trop grand")
-        return 0
-    else:
-    """
-    if cut == True:
-        nb_f_pos = len(Cut.difference(Cut_verif))
-        nb_f_neg = len(Cut_verif.difference(Cut))
-        print("CUT :", Cut)
-        print("NOIR :", Noir)
+    for j in range(0,split**2):
+        Noir_split, Cut_split, RedCut_split, GreenCut_split, BlueCut_split = [], [], [], [], []
+        for i in range(2,len(Red_split[:,j])):
+            if np.abs(Red_split[i,j]-Red_split[i-1,j]) > seuil_cut:
+                RedCut_split.append(i)
+                # print("Coupure des rouges à la frame ", i+1)
+            if np.abs(Green_split[i,j]-Green_split[i-1,j]) > seuil_cut:
+                GreenCut_split.append(i)
+            if np.abs(Blue_split[i,j]-Blue_split[i-1,j]) > seuil_cut:
+                BlueCut_split.append(i)
 
-    else :
-        nb_f_pos = len(Noir.difference(Noir_verif))
-        nb_f_neg = len(Noir_verif.difference(Noir))
-        print("CUT :", Cut)
-        print("NOIR :", Noir)
+        # Puis on considère que c'est un cut si les variations sont communes à chaque couleur
+        for i in RedCut_split:
+            # if i in GreenCut and i in BlueCut :
+            if Red_split[i,j] < seuil_noir and Green_split[i,j] < seuil_noir and Blue_split[i,j] < seuil_noir :
+                Noir_split.append(i+1)
+            else :
+                Cut_split.append(i+1)
+                
+                # print("Coupure à la frame ", i+1)
+                # print("Red : ",Red[i], "; Green : ", Green[i], "; Blue : ", Blue[i])
+                    
+        # On compare à présent les résultats à la vérité sur terrain
         
-    nb_erreur = nb_f_pos + nb_f_neg
-    Nb_f_pos.append(nb_f_pos)
-    Nb_f_neg.append(nb_f_neg)
-    Nb_erreur.append(nb_erreur)
-    
-    return None
+        Noir_split = set(Noir_split)
+        Cut_split = set(Cut_split)
+        #print()
+        #print('Cut :', sorted(Cut_split))
+        #print('Noir :', sorted(Noir_split))
+
+        if cut == True:
+            nb_f_pos[j] = len(Cut_split.difference(Cut_verif))
+            nb_f_neg[j] = len(Cut_verif.difference(Cut_split))
+            print("CUT :", Cut_split[j])
+            print("NOIR :", Noir_split[j])
+
+        else :
+            nb_f_pos[j] = len(Noir_split.difference(Noir_verif))
+            nb_f_neg[j] = len(Noir_verif.difference(Noir_split))
+            #print("CUT :", Cut_split[j])
+            #print("NOIR :", Noir_split[j])
+            
+        nb_erreur[j] = nb_f_pos[j] + nb_f_neg[j]
+        
+    Nb_f_pos_split = np.append(Nb_f_pos_split, [nb_f_pos], axis = 0)
+    Nb_f_neg_split = np.append(Nb_f_neg_split, [nb_f_neg], axis = 0)
+    Nb_erreur_split = np.append(Nb_erreur_split, [nb_erreur], axis = 0)
+    return Nb_f_pos_split, Nb_f_neg_split, Nb_erreur_split
+
 #%%
 def Analyse_greyscale(Nb_f_pos_split, Nb_f_neg_split, Nb_erreur_split, seuil_cut, seuil_noir, cut):
     nb_f_pos =  [0 for i in range(split**2)]
@@ -160,16 +150,17 @@ def Analyse_greyscale(Nb_f_pos_split, Nb_f_neg_split, Nb_erreur_split, seuil_cut
             if np.abs(Grey_split[i,j]-Grey_split[i-1,j]) > seuil_cut:
                 if Grey_split[i,j] < seuil_noir :
                     Noir_split.append(i+1)
-                    # print("Noir à la frame ", i+1)
+                    #print("Noir à la frame ", i+1)
                 else :
                     Cut_split.append(i+1)
-                #print("Coupure à la frame ", i+1)
+                    #print("Coupure à la frame ", i+1)
 
         # On compare à présent les résultats à la vérité sur terrain
         Noir_split = set(Noir_split)
         Cut_split = set(Cut_split)
-        #print('Cut :', Cut_split)
-        #print('Noir :', Noir_split)
+        print()
+        print('Cut :', sorted(Cut_split))
+        print('Noir :', sorted(Noir_split))
         """
         if len(Cut_split) == 0 :
             print("Erreur : seuil trop grand")
@@ -219,9 +210,9 @@ else:
     Cut_verif = {52, 142, 163, 187, 200, 221, 248, 256, 268, 307, 485, 526, 561, 582, 595, 615, 635, 664, 690, 705, 720, 746, 821, 853, 903, 956, 975, 998, 1027, 1062, 1099, 1120, 1144, 1177, 1220, 1255, 1293, 1335, 1367, 1444, 1582, 1655, 1735, 1812, 1871, 1895, 1909, 1960, 2016, 2106, 2147, 2184, 2243, 2487, 2526, 2617, 2688, 2775, 2808, 2829, 2858, 2881, 2917, 2934, 2962, 2978, 3011, 3086, 3179}
     Noir_verif = {42, 552, 812, 1573, 2007, 2766, 3277}
     
-    GreyScale = True
+    GreyScale = False
     displayEvo = False
-    split = 4
+    split = 3
     cut = True
 
     if GreyScale :
@@ -248,13 +239,15 @@ else:
                     axs[i, j].plot(Xrange, Green_split[:,i+j*2], label = "Vert", color = 'green')
                     axs[i, j].plot(Xrange, Blue_split[:,i+j*2], label = "Bleu", color = 'blue')
                     # plt.savefig('Evolution des niveaux de gris.png', dpi=300)
+                    
+#%% Détection des erreurs
 
     Nb_f_pos_split = np.zeros([0,split**2])
     Nb_f_neg_split = np.zeros([0,split**2])
     Nb_erreur_split = np.zeros([0,split**2])
     
-    Nb_echa = 100
-    Seuils = np.linspace(10, 45, Nb_echa)
+    Nb_echa = 200
+    Seuils = np.linspace(5, 50, Nb_echa)
     
     if cut :
         seuil_noir = 6
@@ -265,7 +258,7 @@ else:
             else :
                 Nb_f_pos_split, Nb_f_neg_split, Nb_erreur_split = Analyse_colour(Nb_f_pos_split, Nb_f_neg_split, Nb_erreur_split, seuil_cut, seuil_noir, cut)
     else :
-        seuil_cut = 18
+        seuil_cut = 15
         for seuil_noir in Seuils:
             # print("Seuil noir :", seuil_noir)
             if GreyScale :
